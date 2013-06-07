9ebb222b25178e37cb43916b55bb52f0dcac72a3
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-06-07 15:27:13 +0530

    initial commit

---
+++ client/admin.js
@@ -0,0 +1,9 @@
+Meteor.loginAsAdmin = function(password, callback) {
+  
+  var loginRequest = {admin: true, password: password};
+
+  Accounts.callLoginMethod({
+    methodArguments: [loginRequest],
+    userCallback: callback
+  });
+};
