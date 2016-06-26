b45a5f33262586b47ca584391f21a3bf37b65dc7
Marc Nitzsche <marc.nitzsche@live.de>
2016-06-26 16:30:49 +0200

    Update project to Meteor 1.3

--- client/admin.js
+++
@@ -1,9 +0,0 @@
-Meteor.loginAsAdmin = function(password, callback) {
-  
-  var loginRequest = {admin: true, password: password};
-
-  Accounts.callLoginMethod({
-    methodArguments: [loginRequest],
-    userCallback: callback
-  });
-};
---
+++ server/server.js
@@ -0,0 +1,38 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+
+Accounts.registerLoginHandler(function(loginRequest) {
+    if (!loginRequest.admin) {
+        return undefined;
+    }
+
+    if (loginRequest.password != 'admin-password') {
+        return {error: Meteor.Error(403, 'Incorrect Admin Password')};
+    }
+
+    var userId = null;
+    var user = Meteor.users.findOne({
+        username: 'admin'
+    });
+    if (!user) {
+        userId = Meteor.users.insert({
+            username: 'admin'
+        });
+    } else {
+        userId = user._id;
+    }
+
+    //creating the token and adding to the user
+    var stampedToken = Accounts._generateStampedLoginToken();
+    Meteor.users.update(userId, {
+        $push: {
+            'services.resume.loginTokens': stampedToken
+        }
+    });
+
+    //sending token along with the userId
+    return {
+        id: userId,
+        token: stampedToken.token
+    }
+});
